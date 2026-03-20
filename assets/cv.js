0aeefbf5b65829bc93bccd2d5bf422054011443c
majeurbil <2497570@csfoy.ca>
2026-03-20 00:22:54 -0400

    v4

--- assets/cv.js
+++ assets/cv.js
@@ -1,6 +1,4 @@
-// On attend que le DOM soit chargé
 document.addEventListener("DOMContentLoaded", function() {
-    // On découpe l'adresse pour qu'un robot ne puisse pas lire le mot complet
     const user = "billy.halle.bh";
     const domain = "gmail.com";
     const maVraieAdresse = user + "@" + domain;
@@ -8,14 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const emailLink = document.querySelector('a.email');
 
     if (emailLink) {
-        // 3. On remplace le texte "enable JavaScript..." par ton adresse
         emailLink.textContent = maVraieAdresse;
-
-        // 4. On règle le lien pour qu'il ouvre un nouveau message vers ton Gmail
         emailLink.setAttribute('href', 'mailto:' + maVraieAdresse);
     }
 
-    // Interrupteur mode sombre / clair
     function lightSwitch(e) {
         var darkClass = "dark";
         if (e.target.checked) {
